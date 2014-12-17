0232d1846e82faac0be01f5c7b0ee5f7ce37ca07
nguyenduthpt@gmail.com <nguyenduthpt@gmail.com@c717a811-2d4c-6926-82c8-c8339f451f0c>
2014-12-17 18:32:06 +0000

    Them quanli

---
+++ trunk/quanli/truong.php
@@ -0,0 +1,74 @@
+﻿<?php
+
+require_once "config.php";
+$link = mysqli_connect($db_host, $db_user, $db_pass);
+mysqli_select_db($link,$db_name);
+mysqli_query($link,"SET NAMES utf8"); //connect in decode utf8
+//define("DEBUG",TRUE);
+echo "<html><head><title>Trường học</title></head><body>";
+if (isset($_POST['search'])) { //tim kiem
+	$url=$_POST['url'];
+	$email=$_POST['email'];
+	
+	$sql="";
+	if ($url)
+		$sql.="trangweb='$url'";
+	if ($email) {
+		if ($sql) $sql.= " or ";
+		$sql.= "email='$email'";
+	}
+	if ($sql) {
+		$sql="select * from truonghoc where ".$sql;
+		$ret=mysqli_query($link,$sql);
+		if ($row=mysqli_fetch_array($ret)) {
+			echo "Đã có:";
+			echo "<br/>Tên: ".$row['tentruong'];
+			echo "<br/>Địa chỉ: ".$row['diachi'];
+			echo "<br/>Điện thoại: ".$row['sodienthoai'];
+			echo "<br/>Email: ".$row['email'];
+			echo "<br/>Trang web: ".$row['trangweb'];
+			echo "<br/><a href='truong.php'>Thêm trường khác</a>";
+			redirect("truong.php",10);
+		} else {
+			echo "Chưa có. Thêm vào: <form action='truong.php' method='post'>";
+			echo "<br/>Tên trường: <input type='text' name='ten' size='100'><br/>";
+			echo "<br/>Địa chỉ: <input type='text' name='diachi' size='100'><br/>";
+			echo "<br/>Điện thoại: <input type='text' name='dienthoai' size='11'><br/>";
+			echo "<br/>Email: <input type='text' name='email' size='50' value='$email'><br/>";
+			echo "<br/>Trang web: <input type='text' name='web' size='150' value='$url'><br/>";
+			echo "<br/><input type='submit' value='Thêm' name='add'>";
+			echo "</form>";
+		}
+	} else {
+		echo "Chưa nhập địa chỉ website hoặc email";
+		redirect("truong.php",2);
+	}
+} else if (isset($_POST['add'])) { //them moi
+	$url=$_POST['web'];
+	$email=$_POST['email'];
+	$ten=$_POST['ten'];
+	$diachi=$_POST['diachi'];
+	$dienthoai=$_POST['dienthoai'];
+	$ret=mysqli_query($link,"insert into truonghoc (tentruong,diachi,sodienthoai,email,trangweb,lucnao) value ('$ten','$diachi','$dienthoai','$email','$url',NOW())");
+	echo "Đã thêm";
+	echo "<br/><a href='truong.php'>Thêm trường khác</a>";
+	redirect("truong.php",10);
+} else {
+	echo "Tìm kiếm: <form action='truong.php' method='post'>";
+	echo "URL: <input type='text' name='url' size='200'><br/><br/>";
+	echo "Email: <input type='text' name='email' size='50'><br/>";
+	echo "<br/><input type='submit' value='Tìm' name='search'>";
+	echo "</form>";
+	
+}
+echo "</body></html>";
+
+
+function redirect($location, $delaytime = 0) {
+    if ($delaytime>0) {    
+        header( "refresh: $delaytime; url='".str_replace("&amp;", "&", $location)."'" );
+    } else {
+        header("Location: ".str_replace("&amp;", "&", $location));
+    }    
+}
+?>
---
+++ trunk/quanli/tulieu.php
@@ -0,0 +1,95 @@
+﻿<?php
+
+require_once "config2.php";
+$link = mysqli_connect($db_host, $db_user, $db_pass);
+mysqli_select_db($link,$db_name);
+mysqli_query($link,"SET NAMES utf8"); //connect in decode utf8
+//define("DEBUG",TRUE);
+echo "<html><head><title>Tư liệu</title></head><body>";
+if (!isset($_GET['sk'])) {
+	
+	$skid=intval($_GET['id']);
+	if (isset($_GET['delete'])) {
+		$sql="delete from cactulieu where id=$skid;";
+		$ret=mysqli_query($link,$sql);
+		redirect("?",3);
+	}
+	if (isset($_POST['sua'])) {
+		$theloai=$_POST['loaitulieu'];
+		$url=$_POST['url'];
+		$ghichu=htmlspecialchars($_POST['ghichu']);
+		$sudung=isset($_POST['sudung'])?1:0;
+		$sql="update cactulieu set theloai='$theloai',url='$url',tieude='$ghichu',sudung=$sudung where id=$skid";
+		echo $sql;
+		$ret=mysqli_query($link,$sql);
+		redirect("tulieu.php?id=$skid",5);
+	}
+	else {
+		$sql="select * from cactulieu where id=$skid";
+		$ret=mysqli_query($link,$sql);
+		if ($row=mysqli_fetch_array($ret)) {
+			//echo "<div>".$row['loai'].". <a href='tulieu.php?id=".$row['id']."'>".$row['tieude']."</a> [<a href='".$row['url']."' target='_blank'>Xem</a>]</div>";
+			echo "<hr/>Sửa tư liệu: <form action='tulieu.php?id=$skid' method='post'>";
+			echo "Thể loại: <select name='loaitulieu'>";
+			$sql="select * from loaitulieu";
+			$ret = mysqli_query($link,$sql);
+			while ($row2=mysqli_fetch_array($ret)) {
+				echo "<option value='".$row2['id']."' ".($row2['id']==$row['theloai']?" selected":"").">".$row2['tenloai']."</option>";
+			}
+			echo "</select><br/><br/>";
+			echo "Tiêu đề: <input type='text' name='ghichu' size='200' value='".$row['tieude']."'><br/>";
+			echo "URL: <input type='text' name='url' size='200' value='".$row['url']."'><br/><br/>";
+			echo "<br/><input type='checkbox' name='sudung' ".($row['sudung']?"checked":"").">Sử dụng";
+			echo "<br/><input type='submit' value='Sửa' name='sua'>";
+			echo "</form>";
+			echo "<br/><a href='tulieu.php?delete&id=$skid' onclick=\"return (confirm('Bạn muốn xóa tư liệu này'))\">Xoá tư liệu này</a>";
+		}
+			
+	}
+} else {
+	$skid=intval($_GET['sk']);
+	if (isset($_POST['ghi'])) {
+		$theloai=$_POST['loaitulieu'];
+		$url=$_POST['url'];
+		$ghichu=htmlspecialchars($_POST['ghichu']);
+		//$sudung=isset($_POST['sudung']);
+		$sql="insert into cactulieu (sk,theloai,url,luctao,tieude) value ('$skid','$theloai','$url',NOW(),'$ghichu')";
+		$ret=mysqli_query($link,$sql);
+		redirect("tulieu.php?sk=$skid",5);
+	}  else {
+		$sql="select * from cacsukien where id=$skid limit 1";
+		$ret=mysqli_query($link,$sql);
+		if ($row=mysqli_fetch_array($ret)) {
+			echo "<div>".$row['ngay']."/".$row['thang']."/".$row['nam']." (".$row['sudung']."): ".$row['sukien']."</div><hr/>";
+		}
+		$sql="select * from cactulieu where sk=$skid";
+		$ret=mysqli_query($link,$sql);
+		while ($row=mysqli_fetch_array($ret)) {
+			echo "<div>".$row['theloai'].". <a href='tulieu.php?id=".$row['id']."'>".$row['tieude']."</a> [<a href='".$row['url']."' target='_blank'>Xem</a>]</div>";
+		}
+		echo "<hr/>Thêm tư liệu: <form action='tulieu.php?sk=$skid' method='post'>";
+		echo "Thể loại: <select name='loaitulieu'>";
+		$sql="select * from loaitulieu";
+		$ret = mysqli_query($link,$sql);
+		while ($row=mysqli_fetch_array($ret)) {
+			echo "<option value='".$row['id']."'>".$row['tenloai']."</option>";
+		}
+		echo "</select><br/><br/>";
+		echo "Tiêu đề: <input type='text' name='ghichu' size='200'><br/>";
+		echo "URL: <input type='text' name='url' size='200'><br/><br/>";
+		
+		echo "<br/><input type='submit' value='Thêm' name='ghi'>";
+		echo "</form>";
+	}
+}
+echo "</body></html>";
+
+
+function redirect($location, $delaytime = 0) {
+    if ($delaytime>0) {    
+        header( "refresh: $delaytime; url='".str_replace("&amp;", "&", $location)."'" );
+    } else {
+        header("Location: ".str_replace("&amp;", "&", $location));
+    }    
+}
+?>
